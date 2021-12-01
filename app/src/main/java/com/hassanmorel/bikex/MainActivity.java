67418dd5e8ee9f8bec0be45752c8775a42a889db
hassano <Oualid.hassan.pro@gmail.com>
2021-12-01 15:11:09 +0100

    add permission

--- app/src/main/java/com/hassanmorel/bikex/MainActivity.java
+++ app/src/main/java/com/hassanmorel/bikex/MainActivity.java
@@ -1,8 +1,23 @@
 package com.hassanmorel.bikex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.hassanmorel.bikex.api.ApiClient;
+import com.hassanmorel.bikex.api.ApiInterface;
+import com.hassanmorel.bikex.api.models.ApiFeature;
+import com.hassanmorel.bikex.api.models.ApiRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getAllFeatures();
+    }
+
+    private List<ApiFeature> getAllFeatures() {
+        List<ApiFeature> features = new ArrayList<>();
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<ApiRequest> call = apiService.getFeatures();
+
+        call.enqueue(new Callback<ApiRequest>() {
+            @Override
+            public void onResponse(@NonNull Call<ApiRequest> call, @NonNull Response<ApiRequest> response) {
+                Log.d("BikeX", "Response");
+                assert response.body() != null;
+
+                response.body().getFeatures().forEach(feature -> Log.d("BikeX", feature.toString()));
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ApiRequest> call, @NonNull Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+        return features;
     }
 }
